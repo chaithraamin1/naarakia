22f9e724914524fda47c806aabf52352f66ef1d4
chaithraamin1 <chaithra.amin98@gmail.com>
2019-11-28 07:59:41 +0530

    carousel-task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { from } from 'rxjs';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
